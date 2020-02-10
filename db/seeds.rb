2bda0a9817d7cb3442d9bfe31252037a5ee1d7fb
Sonata <ostarablue@gmail.com>
2020-02-10 17:19:30 -0500

    add keys to seed data

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+require_relative '../config/environment.rb'
+require 'pry'
+
+
+cust1 = Customer.create(name:"Jason", username: "JasonJason")
+cust2 = Customer.create(name:"Sonata", username: "SonataSonata")
+cust3 = Customer.create(name:"Yo", username: "YoYo")
+
+game1 = Game.create(name: "Fortnite", price: 10.00, rating: 4.9, category: "Action", company: "EpicGames")
+game2 = Game.create(name: "Call of Duty", price: 7.99, rating:4.5, category:"Action", company:"Steam")
+game3 = Game.create(name: "League of Legends", price: 0.00, rating: 4.0, category: "MOBA", company:"RiotGames")
+
+# profile = Profile.create(cust1)
+# binding.pry
+# 0
